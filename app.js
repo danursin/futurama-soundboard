222cdf64e92b0a53b28a5452313162607ca0cf28
danursin <danursin@gmail.com>
2021-12-24 00:06:48 -0600

    dragging effect

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
 $(() => {
     const playSound = (name) => {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             const src = `sounds/${name}.mp3`;
             const audio = new Audio(src);
             audio.addEventListener("ended", () => resolve());
+            audio.addEventListener("error", () => reject());
             audio.play();
         });
     };
 
-    $(".button-sound").click(async (e) => {
-        const elem = $(e.currentTarget);
-        const character = elem.data("character");
-        await playSound(character);
-    });
-
-    $(".col").draggable({
-        revert: "invalid",
-        opacity: 1,
-        zIndex: 3,
-        revertDuration: 300,
-        start: function (event) {
-            console.log(event);
-            $(this).next().css("color", "red");
-        }
-    });
-
-    // $("button").droppable({
-    //     hoverClass: "drop",
-    //     tolerance: "pointer",
-    //     drop: function (event, ui) {
-    //         console.log(event);
-    //     }
-    // });
+    $(".button-sound")
+        .click(async (e) => {
+            const elem = $(e.currentTarget);
+            const character = elem.data("character");
+            await playSound(character);
+        })
+        .draggable({
+            revert: true,
+            opacity: 0.8,
+            zIndex: 1,
+            revertDuration: 0
+        })
+        .droppable({
+            hoverClass: "drop",
+            tolerance: "pointer",
+            drop: async (event, ui) => {
+                const sourceCharacter = ui.draggable.attr("data-character");
+                const destinationCharacter = $(event.target).data("character");
+                const name = `${sourceCharacter}_${destinationCharacter}`;
+                console.log(`Dropped ${sourceCharacter} to ${destinationCharacter}`);
+                try {
+                    await playSound(name);
+                } catch (err) {
+                    await playSound("no_sound");
+                }
+            }
+        });
 });
