23a0ceed55ca83cc5261b118b1293c7f094dabe5
danursin <danursin@gmail.com>
2021-12-24 22:38:29 -0600

    restore draggable

--- app.js
+++ app.js
@@ -14,27 +14,29 @@ $(() => {
         const character = elem.data("character");
         await playSound(character);
     });
-    // .draggable({
-    //     delay: 200,
-    //     opacity: 0.8,
-    //     revert: true,
-    //     revertDuration: 0,
-    //     zIndex: 1
-    // })
-    // .droppable({
-    //     hoverClass: "drop",
-    //     tolerance: "pointer",
-    //     drop: async (event, ui) => {
-    //         const sourceCharacter = ui.draggable.attr("data-character");
-    //         const destinationCharacter = $(event.target).data("character");
-    //         const [char1, char2] = [sourceCharacter, destinationCharacter].sort();
-    //         const name = `${char1}_${char2}`;
-    //         console.log(`Dropped ${sourceCharacter} to ${destinationCharacter}`);
-    //         try {
-    //             await playSound(name);
-    //         } catch (err) {
-    //             await playSound("no_sound");
-    //         }
-    //     }
-    // });
+
+    $(".button-sound").draggable({
+        delay: 200,
+        opacity: 0.8,
+        revert: true,
+        revertDuration: 0,
+        zIndex: 1
+    });
+
+    $(".button-sound").droppable({
+        hoverClass: "drop",
+        tolerance: "pointer",
+        drop: async (event, ui) => {
+            const sourceCharacter = ui.draggable.attr("data-character");
+            const destinationCharacter = $(event.target).data("character");
+            const [char1, char2] = [sourceCharacter, destinationCharacter].sort();
+            const name = `${char1}_${char2}`;
+            console.log(`Dropped ${sourceCharacter} to ${destinationCharacter}`);
+            try {
+                await playSound(name);
+            } catch (err) {
+                await playSound("no_sound");
+            }
+        }
+    });
 });
